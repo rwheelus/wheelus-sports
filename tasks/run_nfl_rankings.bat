d58db38a9f5cc96e48e311ef22fd06a4d087f140
rwheelus <robinwheelus@gmail.com>
2025-11-11 19:45:40 -0600

    Remove scripts and tasks folders from repository

--- tasks/run_nfl_rankings.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM Activate virtual environment
-call C:\Users\robin\anaconda3\Scripts\activate.bat collegefootball
-
-REM Run your NFL rankings script
-python "C:\Users\robin\.spyder-py3\NFL Ranking Violations -- two stage ILP.py"
-
-REM Optional: log output to a file so you can check later
-python "C:\Users\robin\.spyder-py3\NFL Ranking Violations -- two stage ILP.py" >> "C:\Users\robin\wheelus-sports\scripts\nfl_log.txt" 2>&1
--- tasks/run_nhl_rankings.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM Activate virtual environment
-call C:\Users\robin\anaconda3\Scripts\activate.bat collegefootball
-
-REM Run your NHL rankings script
-python "C:\Users\robin\.spyder-py3\NHL Ranking API.py"
-
-REM Optional: log output to a file so you can check later
-REM python "C:\Users\robin\.spyder-py3\NHL Ranking API.py" >> "C:\Users\robin\wheelus-sports\scripts\nhl_log.txt" 2>&1
